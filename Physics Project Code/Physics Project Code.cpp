ef2a4e4ce45be0eece41b995665c04b0bf03f09e
Yusmen <85336773+YIOsman20@users.noreply.github.com>
2021-11-23 19:30:45 +0200

    Added two functions
    Added two functions for equal acceleration and free falling

--- Physics Project Code/Physics Project Code.cpp
+++ Physics Project Code/Physics Project Code.cpp
@@ -5,6 +5,20 @@
 
 using namespace std;
 
+// Fucntion to find the speed using equal acceleration formula
+int equalAcceleration(int Vo, int a, int t)
+{
+    
+    return Vo + a * t;
+    // Returns the value
+}
+// Function to find the speed using free faling formula
+float freeFaling(float t)
+{
+    float g = 9.8;
+    return (g * t * t) / 2;
+}
+
 int main()
 {
 	int number1;
