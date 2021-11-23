c4a8c0aa05341f7acaa6b2df85695f1eda44deab
Kliment Tenev <85335524+KGTenev20@users.noreply.github.com>
2021-11-23 19:42:43 +0200

    Added more functions

--- Physics Project Code/Physics Project Code.cpp
+++ Physics Project Code/Physics Project Code.cpp
@@ -12,11 +12,24 @@ int equalAcceleration(int Vo, int a, int t)
     return Vo + a * t;
     // Returns the value
 }
+// Fucntion to find the path length using equal acceleration path formula
+int equalAccelerationPath(int Vo, int a, int t)
+{
+
+    return (Vo + a * t)/2;
+    // Returns the value
+}
+// Function to find the path length using free faling path formula
+float freeFalingPath(float t)
+{
+    float g = 9.8;
+    return (g * t * t) / 2;
+}
 // Function to find the speed using free faling formula
 float freeFaling(float t)
 {
     float g = 9.8;
-    return (g * t * t) / 2;
+    return (g * t);
 }
 
 int main()
