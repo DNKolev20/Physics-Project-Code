964007594273bb3bc9c323f8aed6769cc5eba606
Denis Kolev <85335546+DNKolev20@users.noreply.github.com>
2021-11-27 22:37:56 +0200

    Edited comments and function names

--- Physics Project Code/Physics Project Code.cpp
+++ Physics Project Code/Physics Project Code.cpp
@@ -5,106 +5,124 @@
 
 using namespace std;
 
-//Function to calculate how much a person weights on other planets
+//Function that calculates how much a person weights on other planets
 float weightOnOtherPlanets(float weightOnEarth, int planet)
 {
-
     switch (planet)
     {
     case 1:
-        return weightOnEarth * 27.01;// calculates how much you weight on the Sun
+        return weightOnEarth * 27.01; // calculates how much you weight on the Sun
         break;
     case 2:
-        return weightOnEarth * 0.38;// calculates how much you weight on Mercury
+        return weightOnEarth * 0.38; // calculates how much you weight on Mercury
         break;
     case 3:
-        return weightOnEarth * 0.91;// calculates how much you weight on Venus
+        return weightOnEarth * 0.91; // calculates how much you weight on Venus
         break;
     case 4:
-        return weightOnEarth * 0.166;// calculates how much you weight on the Moon
+        return weightOnEarth * 0.166; // calculates how much you weight on the Moon
         break;
     case 5:
-        return weightOnEarth * 0.38;// calculates how much you weight on Mars
+        return weightOnEarth * 0.38; // calculates how much you weight on Mars
         break;
     case 6:
-        return weightOnEarth * 2.34;// calculates how much you weight on Jupiiter
+        return weightOnEarth * 2.34; // calculates how much you weight on Jupiiter
         break;
-
     case 7:
-        return weightOnEarth * 1.06;// calculates how much you weight on Saturn
+        return weightOnEarth * 1.06; // calculates how much you weight on Saturn
         break;
     case 8:
-        return weightOnEarth * 0.92;// calculates how much you weight on Uranus
+        return weightOnEarth * 0.92; // calculates how much you weight on Uranus
         break;
     case 9:
-        return weightOnEarth * 1.19;// calculates how much you weight on Neptune
+        return weightOnEarth * 1.19; // calculates how much you weight on Neptune
         break;
     case 10:
-        return weightOnEarth * 0.06;// calculates how much you weight on Pluto
+        return weightOnEarth * 0.06; // calculates how much you weight on Pluto
         break;
     default:
-        cout << "Invalid input" << endl;// What to do if the inuput is different than the cases
+        cout << "Invalid input" << endl; // What to do if the input is different from the cases
         return 0;
     }
-
 }
 
-// Fucntion to find the speed using equal acceleration formula
+
+
+
+// Function that finds the speed using the equal acceleration formula
 int equalAccelerationSpeed(int Vo, int a, int t)
 {
 
     return Vo + a * t;
     // Returns the value
 }
-// Fucntion to find the starting speed using equal acceleration formula
+
+
+// Function that finds the starting speed using the equal acceleration formula
 int equalAccelerationStartingSpeed(int V, int a, int t)
 {
 
     return V - a * t;
     // Returns the value
 }
-// Fucntion to find the acceleration using equal acceleration formula
+
+
+// Function that finds the acceleration using the equal acceleration formula
 int equalAccelerationAcceleration(int V, int Vo, int t)
 {
 
     return (V - Vo) / t;
     // Returns the value
 }
-// Fucntion to find the time using equal acceleration formula
+
+
+// Function that finds the time using the equal acceleration formula
 int equalAccelerationTime(int V, int Vo, int a)
 {
 
     return (V - Vo) / a;
     // Returns the value
 }
-// Fucntion to find the path length using equal acceleration path formula
+
+
+// Function that finds the path length using the equal acceleration path formula
 int equalAccelerationPath(int Vo, int a, int t)
 {
-
     return (Vo + a * t)/2;
     // Returns the value
 }
-// Function to find the path length using free faling path formula
-float freeFalingPath(float t)
+
+
+
+
+// Function that finds the path length using the free falling path formula
+float freeFallingPath(float t)
 {
     float g = 9.8;
     return (g * t * t) / 2;
-	 // Returns the value
+    // Returns the value
 }
-// Function to find the speed using free faling formula
-float freeFaling(float t)
+
+
+// Function that finds the speed using the free falling formula
+float freeFalling(float t)
 {
     float g = 9.8;
     return (g * t);
-	 // Returns the value
+    // Returns the value
 }
-// Function to find the speed using free faling formula
-float freeFalingTime(float V)
+
+
+// Function that finds the speed using the free falling formula
+float freeFallingTime(float V)
 {
     float g = 9.8;
     return (V / g);
-	 // Returns the value
+    // Returns the value
 }
+
+
+
 
 int main()
 {
